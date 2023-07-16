f21c6dd588caf06595c90a287046c0d5d45aa2f2
Joruno-w <1710642275@qq.com>
2023-07-17 00:00:37 +0800

    feat: add config file

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,60 @@
-import { describe, expect, it } from 'vitest'
+// import { describe, expect, it } from "vitest";
+// import upbot, { getConfig } from "../src";
 
-describe('should', () => {
-  it('exported', () => {
-    expect(1 + 1).toEqual(2)
-  })
-})
+// describe("test upbot", () => {
+//   it("Get Config", () => {
+//     expect(getConfig()).toMatchInlineSnapshot(`
+//       {
+//         "directory": "~/Desktop/zhuanzhuan",
+//         "name": "wangshengliang",
+//         "pwd": "WaNg79565713!",
+//         "url": "https://beetle.zhuanspirit.com/",
+//       }
+//     `);
+//   });
+//   it("Get Cookies", async () => {
+//     expect(await upbot()).toMatchInlineSnapshot();
+//   });
+// });
+
+import { afterAll, beforeAll, describe, expect, test } from "vitest";
+import { preview } from "vite";
+import type { PreviewServer } from "vite";
+import puppeteer from "puppeteer";
+import type { Browser, Page } from "puppeteer";
+
+describe("basic", async () => {
+  let server: PreviewServer;
+  let browser: Browser;
+  let page: Page;
+
+  beforeAll(async () => {
+    server = await preview({ preview: { port: 3000 } });
+    browser = await puppeteer.launch();
+    page = await browser.newPage();
+  });
+
+  afterAll(async () => {
+    await browser.close();
+    await new Promise<void>((resolve, reject) => {
+      server.httpServer.close((error) => (error ? reject(error) : resolve()));
+    });
+  });
+  // TODO
+  test.skip("should have the correct title", async () => {
+    try {
+      await page.goto("http://localhost:3000");
+      const button = (await page.$("#btn"))!;
+      expect(button).toBeDefined();
+
+      let text = await page.evaluate((btn) => btn.textContent, button);
+      expect(text).toBe("Clicked 0 time(s)");
+
+      await button.click();
+      text = await page.evaluate((btn) => btn.textContent, button);
+    } catch (e) {
+      console.error(e);
+      expect(e).toBeUndefined();
+    }
+  }, 60_000);
+});
